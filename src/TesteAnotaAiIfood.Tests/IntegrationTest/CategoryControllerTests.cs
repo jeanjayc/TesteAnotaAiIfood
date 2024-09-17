42363e831735f2d102e60deab01c7ac040a318c6
jean Carlos <jeanmaster_k66@hotmail.com>
2024-09-16 22:11:33 -0300

    add projeto de teste de integraçao

---
+++ src/TesteAnotaAiIfood.Tests/IntegrationTest/CategoryControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace TesteAnotaAiIfood.Tests.IntegrationTest
+{
+    public class CategoryControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CategoryControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Category_GetAll_ShouldReturnAllCategorys()
+        {
+            //Arrange
+            var _client = _factory.CreateClient();
+
+            //Act
+            var response = await _client.GetAsync("api/Category/GetAll");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
